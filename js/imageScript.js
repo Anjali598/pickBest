09529f15b906a0db3750dcf68146a66078505384
Anjali <34765826+Anjali598@users.noreply.github.com>
2020-06-15 16:26:34 +0530

    Add files via upload

--- js/imageScript.js
+++ js/imageScript.js
@@ -13,68 +13,68 @@
 			
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-1"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-2"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-3"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-4"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-5"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-6"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-7"
 			};
 			
 			imageArray[7] = {
-				src : "D:/website-Task/Pick Best/img/8.jpg",
+				src : "img/8.jpg",
 				caption : "Image caption for image-8"
 			};
 			
 			imageArray[8] = {
-				src : "D:/website-Task/Pick Best/img/9.jpg",
+				src : "img/9.jpg",
 				caption : "Image caption for image-9"
 			};
 			
 			imageArray[9] = {
-				src : "D:/website-Task/Pick Best/img/10.jpg",
+				src : "img/10.jpg",
 				caption : "Image caption for image-10"
 			};
 			imageArray[10] = {
-				src : "D:/website-Task/Pick Best/img/11.jpg",
+				src : "img/11.jpg",
 				caption : "Image caption for image-11"
 			};
 			imageArray[11] = {
-				src : "D:/website-Task/Pick Best/img/12.jpg",
+				src : "img/12.jpg",
 				caption : "Image caption for image-12"
 			};
 			imageArray[12] = {
-				src : "D:/website-Task/Pick Best/img/13.jpg",
+				src : "img/13.jpg",
 				caption : "Image caption for image-13"
 			};
 			imageArray[13] = {
-				src : "D:/website-Task/Pick Best/img/14.jpg",
+				src : "img/14.jpg",
 				caption : "Image caption for image-14"
 			};
 			imageArray[14] = {
-				src : "D:/website-Task/Pick Best/img/15.jpg",
+				src : "img/15.jpg",
 				caption : "Image caption for image-15"
 			};
 			
@@ -106,41 +106,41 @@
 			
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-02"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-03"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-07"
 			};
 			imageArray[7] = {
-				src : "D:/website-Task/Pick Best/img/8.jpg",
+				src : "img/8.jpg",
 				caption : "Image caption for image-08"
 			};
 			imageArray[8] = {
-				src : "D:/website-Task/Pick Best/img/9.jpg",
+				src : "img/9.jpg",
 				caption : "Image caption for image-09"
 			};
 			var fc = containner.firstChild;
@@ -170,31 +170,31 @@
 			
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/9.jpg",
+				src : "img/9.jpg",
 				caption : "Image caption for image-09"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/10.jpg",
+				src : "img/10.jpg",
 				caption : "Image caption for image-10"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/11.jpg",
+				src : "img/11.jpg",
 				caption : "Image caption for image-11"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/12.jpg",
+				src : "img/12.jpg",
 				caption : "Image caption for image-12"
 			};
 			
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/14.jpg",
+				src : "img/14.jpg",
 				caption : "Image caption for image-12"
 			};
 			
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/13.jpg",
+				src : "img/13.jpg",
 				caption : "Image caption for image-12"
 			};
 			
@@ -225,33 +225,33 @@
 			
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/13.jpg",
+				src : "img/13.jpg",
 				caption : "Image caption for image-13"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/14.jpg",
+				src : "img/14.jpg",
 				caption : "Image caption for image-14"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-07"
 			};
 			
@@ -282,53 +282,53 @@
 			tempid.className = "powerPoint";
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-02"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-03"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-07"
 			};
 			imageArray[7] = {
-				src : "D:/website-Task/Pick Best/img/8.jpg",
+				src : "img/8.jpg",
 				caption : "Image caption for image-08"
 			};
 			imageArray[8] = {
-				src : "D:/website-Task/Pick Best/img/9.jpg",
+				src : "img/9.jpg",
 				caption : "Image caption for image-09"
 			};
 			imageArray[9] = {
-				src : "D:/website-Task/Pick Best/img/10.jpg",
+				src : "img/10.jpg",
 				caption : "Image caption for image-10"
 			};
 			imageArray[10] = {
-				src : "D:/website-Task/Pick Best/img/11.jpg",
+				src : "img/11.jpg",
 				caption : "Image caption for image-11"
 			};
 			imageArray[11] = {
-				src : "D:/website-Task/Pick Best/img/12.jpg",
+				src : "img/12.jpg",
 				caption : "Image caption for image-12"
 			};
 			
@@ -358,37 +358,37 @@
 			tempid.className = "video";
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-02"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-03"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-07"
 			};
 			imageArray[7] = {
-				src : "D:/website-Task/Pick Best/img/8.jpg",
+				src : "img/8.jpg",
 				caption : "Image caption for image-08"
 			};
 						
@@ -419,45 +419,45 @@
 			tempid.className = "decorsmodels";
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-02"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-03"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			imageArray[6] = {
-				src : "D:/website-Task/Pick Best/img/7.jpg",
+				src : "img/7.jpg",
 				caption : "Image caption for image-07"
 			};
 			imageArray[7] = {
-				src : "D:/website-Task/Pick Best/img/8.jpg",
+				src : "img/8.jpg",
 				caption : "Image caption for image-08"
 			};
 			imageArray[8] = {
-				src : "D:/website-Task/Pick Best/img/9.jpg",
+				src : "img/9.jpg",
 				caption : "Image caption for image-09"
 			};
 			imageArray[9] = {
-				src : "D:/website-Task/Pick Best/img/10.jpg",
+				src : "img/10.jpg",
 				caption : "Image caption for image-10"
 			};
 						
@@ -488,29 +488,29 @@
 			tempid.className = "ecommerce";
 			var imageArray = [];
 			imageArray[0] = {
-				src : "D:/website-Task/Pick Best/img/1.jpg",
+				src : "img/1.jpg",
 				caption : "Image caption for image-01"
 			};
 			imageArray[1] = {
-				src : "D:/website-Task/Pick Best/img/2.jpg",
+				src : "img/2.jpg",
 				caption : "Image caption for image-02"
 			};
 			
 			imageArray[2] = {
-				src : "D:/website-Task/Pick Best/img/3.jpg",
+				src : "img/3.jpg",
 				caption : "Image caption for image-03"
 			};
 			
 			imageArray[3] = {
-				src : "D:/website-Task/Pick Best/img/4.jpg",
+				src : "img/4.jpg",
 				caption : "Image caption for image-04"
 			};
 			imageArray[4] = {
-				src : "D:/website-Task/Pick Best/img/5.jpg",
+				src : "img/5.jpg",
 				caption : "Image caption for image-05"
 			};
 			imageArray[5] = {
-				src : "D:/website-Task/Pick Best/img/6.jpg",
+				src : "img/6.jpg",
 				caption : "Image caption for image-06"
 			};
 			
